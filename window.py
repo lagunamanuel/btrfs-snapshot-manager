fe5989737be915d771ba9e497fa894ebf1ca1817
lagunamanuel <lagunamanuel8@gmail.com>
2026-05-14 21:19:49 +0200

    feat(window): display subvolumes list using Adw.ActionRow

--- window.py
+++ window.py
@@ -2,6 +2,7 @@ import gi
 gi.require_version('Gtk', '4.0')
 gi.require_version('Adw', '1')
 from gi.repository import Gtk, Adw
+import btrfs
 
 class MainWindow(Adw.ApplicationWindow):
     def __init__(self, **kwargs):
@@ -15,6 +16,30 @@ class MainWindow(Adw.ApplicationWindow):
         header = Adw.HeaderBar()
         box.append(header)
 
-        label = Gtk.Label(label='Snapshots will appear here')
-        label.set_vexpand(True)
-        box.append(label)
+        # Scrollable list
+        scroll = Gtk.ScrolledWindow()
+        scroll.set_vexpand(True)
+        box.append(scroll)
+
+        self._list = Gtk.ListBox()
+        self._list.set_selection_mode(Gtk.SelectionMode.NONE)
+        self._list.add_css_class('boxed-list')
+        self._list.set_margin_top(12)
+        self._list.set_margin_bottom(12)
+        self._list.set_margin_start(12)
+        self._list.set_margin_end(12)
+        scroll.set_child(self._list)
+
+        self._load_subvolumes()
+
+    def _load_subvolumes(self):
+        for row in self._list:
+            self._list.remove(row)
+
+        subvolumes = btrfs.list_subvolumes()
+
+        for sv in subvolumes:
+            row = Adw.ActionRow()
+            row.set_title(sv['path'])
+            row.set_subtitle(f"ID: {sv['id']}  •  Gen: {sv['gen']}")
+            self._list.append(row)
